a7c377a8d6730cf4e67327b04061375d8638b08a
HuGo231 <62611110+HuGo231@users.noreply.github.com>
2021-04-02 19:44:22 +0200

    Update TopHead.vue

--- src/components/TopHead.vue
+++ src/components/TopHead.vue
@@ -1,14 +1,23 @@
 <template>
     <header class="top-head">
         <div class="top-head-container">
-            <img v-if="agent.avatarUri" class="top-head-icon" :alt="agent.displayName" :src="agent.avatarUri">
-            <img v-else class="top-head-icon" src="https://console.dialogflow.com/api-client/assets/img/logo-short.png" :alt="agent.displayName">
+            <img class="top-head-icon" src="https://console.dialogflow.com/api-client/assets/img/logo-short.png" :alt="agent.displayName">
             <div class="top-head-info">
                 <div class="top-head-title">{{agent.displayName}}</div>
+                <div class="top-head-subtitle">
+                    
+                </div>
                 <!-- Comment <div class="top-head-subtitle">{{(translations[lang()] && translations[lang()].poweredBy) || translations[config.fallback_lang].poweredBy}} <a target="_blank" rel="noopener noreferrer" href="https://dialogflow.cloud.ushakov.co" aria-hidden="true">Dialogflow Gateway</a></div> -->
-            </div>
+            </div>  
         </div>
-        <slot />
+                <div class="task-box">
+                    <ul>
+                        <h1>Aufgaben</h1>
+                        <li v-for="task in Tasks" v-bind:key="task.id">
+                        {{ task.label }}
+                        </li>
+                    </ul>
+                </div>
     </header>
 </template>
 
@@ -63,6 +72,17 @@ export default {
             type: Object,
             default: null
         }
+    },
+    data(){
+        return {
+            Tasks: [
+                { id: ('task1'), label: 'Frage nach einem Rezept'},
+                { id: ('task2'), label: 'Frage nach einem Rezept'},
+                { id: ('task3'), label: 'Frage nach einem Rezept'},
+                { id: ('task4'), label: 'Frage nach einem Rezept'}
+            ]
+        }
     }
 }
 </script>
+
