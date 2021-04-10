ab52dfd0b9114484ecddf3a29377e49a3c1ffac3
HuGo231 <62611110+HuGo231@users.noreply.github.com>
2021-04-10 18:31:56 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -321,6 +321,7 @@ export default {
                 speech.onend = () => {
                     isPlaying = false;
                 }
+		}
             }
         },
         /* Stop audio speech/playback */
