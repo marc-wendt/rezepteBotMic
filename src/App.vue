d927effc524063e72d7bcf7f6a3e1e6fd67fb522
HuGo231 <62611110+HuGo231@users.noreply.github.com>
2021-04-19 18:26:10 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -336,13 +336,7 @@ export default {
 		
 		
 
-                speech.onend = () => {
-		    console.log("SPEECH-ONEND")
-                    isPlaying = false;
-                    this.$refs.input.speaking = false
-                    //this.$refs.input.wartet= true
-		    this.$refs.input.listen() // AUTOMATISCH AN 2
-                }
+
 		
 		speech.onende = endFunction()
 
