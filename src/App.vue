6540113a485437d184df765a8183ba5c0c435933
HuGo231 <62611110+HuGo231@users.noreply.github.com>
2021-04-26 17:55:35 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -291,7 +291,7 @@ export default {
 
                 if(isSafari){
                    speech.voice = voices[4] // 4 oder 5
-				   	speech.rate = 0.8;
+		   speech.rate = 0.8;
                    console.log("Safari")
                 } else {
                     if(this.$browserDetect.isChrome){
@@ -299,6 +299,7 @@ export default {
                         speech.voice = voices[0]
 			if(navigator.appVersion.indexOf("Mac")!=-1){
 			speech.voice = voices[0]
+			speech.rate = 0.8;
 			}
                     } else if(this.$browserDetect.isFirefox){
                         console.log("Firefox")
